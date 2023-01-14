05f265d1b4981986a68910769e9f16bc86927ef8
sadegh ghasemnejad <ghasemnejad.sadegh@gmail.com>
2023-01-15 00:30:36 +0330

    add components directory add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
+import {
+  HOME_ROUTE,
+  COURSES_ROUTE,
+  COURSE_ROUTE,
+  LOGINREGISTER_ROUTE,
+} from "./routes";
+import Home from "./pages/home";
+import Course from "./pages/course";
+import Courses from "./pages/courses";
+import LoginRegister from "./pages/login-register";
 function App() {
-  return <h1>body</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={HOME_ROUTE} element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path={COURSES_ROUTE} element={<Courses />} />
+        <Route path={COURSE_ROUTE} element={<Course />} />
+        <Route path={LOGINREGISTER_ROUTE} element={<LoginRegister />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+export const HOME_ROUTE = "/home";
+export const COURSES_ROUTE = "/courses";
+export const COURSE_ROUTE = "/course";
+export const LOGINREGISTER_ROUTE = "/login-register";
