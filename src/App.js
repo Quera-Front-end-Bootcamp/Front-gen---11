d5533c37ac07cc9066dfe5d5e1fa49c35d84d0ad
sadegh ghasemnejad <ghasemnejad.sadegh@gmail.com>
2023-01-15 00:32:34 +0330

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { Route, BrowserRouter, Routes, Navigate } from "react-router-dom";
-import {
-  HOME_ROUTE,
-  COURSES_ROUTE,
-  COURSE_ROUTE,
-  LOGINREGISTER_ROUTE,
-} from "./routes";
+import * as ROUTES from "./routes";
 import Home from "./pages/home";
 import Course from "./pages/course";
 import Courses from "./pages/courses";
@@ -13,11 +8,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={HOME_ROUTE} element={<Home />} />
+        <Route path={ROUTES.HOME_ROUTE} element={<Home />} />
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path={COURSES_ROUTE} element={<Courses />} />
-        <Route path={COURSE_ROUTE} element={<Course />} />
-        <Route path={LOGINREGISTER_ROUTE} element={<LoginRegister />} />
+        <Route path={ROUTES.COURSES_ROUTE} element={<Courses />} />
+        <Route path={ROUTES.COURSE_ROUTE} element={<Course />} />
+        <Route path={ROUTES.LOGINREGISTER_ROUTE} element={<LoginRegister />} />
       </Routes>
     </BrowserRouter>
   );
