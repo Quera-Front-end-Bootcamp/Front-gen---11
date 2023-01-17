7c1e88a45cb4ed40acfedcc57da8d7a57d93cdbd
sadegh ghasemnejad <ghasemnejad.sadegh@gmail.com>
2023-01-17 19:17:50 +0330

    ...

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 const Home = () => {
-  return (
-    <div>
-      <Button text="a" color="main" freeSize={true} />
-    </div>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
