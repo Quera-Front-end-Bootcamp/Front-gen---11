dd97171d07d5f3da82382dbe108252862415d956
sadegh ghasemnejad <ghasemnejad.sadegh@gmail.com>
2023-01-20 12:49:42 +0330

    fix rgister button opactiy bug

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,7 +1,9 @@
-const Button = ({ freeSize = false, color, children }) => {
+const Button = ({ freeSize = false, color, opacity = false, children }) => {
   return (
     <button
-      className={`btn ${freeSize && "btn__freesize"} btn__color__${color}`}
+      className={`btn ${freeSize && "btn__freesize"} btn__color__${color} ${
+        opacity && "btn__opacity__" + color
+      }`}
     >
       {children}
     </button>
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -24,9 +24,9 @@ const Header = () => {
       </nav>
 
       <div className="header__login">
-        <Link className="header__loginBtn">ورود</Link>
-        <Button color="main" freeSize="true">
-          <Link className="header__loginBtn">ثبت نام</Link>
+        <Link className="header__login-btn">ورود</Link>
+        <Button color="main" freeSize={true} opacity={true}>
+          <Link className="header__login-btn">ثبت نام</Link>
         </Button>
       </div>
     </header>
