f412817c02dfde8352b1d8f187f0cee4944df792
sadegh <ghasemnejad.sadegh@gmail.com>
2023-01-30 00:28:25 +0330

    fix responsive

--- src/pages/course/index.js
+++ src/pages/course/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import CourseHeader from "../../components/course/header-section";
 
 const Course = () => {
-  return <div>Course</div>;
+  return (
+    <main>
+      <CourseHeader />
+    </main>
+  );
 };
 
 export default Course;
