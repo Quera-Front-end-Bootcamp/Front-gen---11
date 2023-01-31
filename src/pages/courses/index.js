424823357b04c5269c9b8c5e3581687f81285af2
Siavash <siavash.aghazadeh@gmail.com>
2023-01-31 15:59:47 +0330

    add components to courses page

--- src/pages/courses/index.js
+++ src/pages/courses/index.js
@@ -1,8 +1,20 @@
 import React from "react";
-import CourseList from "../../components/courses/CourseList"
+import CourseList from "../../components/courses/CourseList";
+import FooterSection from "../../components/footer";
+import Header from "../../components/UI/Header";
 
 const Courses = () => {
-  return <CourseList />;
+  return (
+    <>
+      <Header />
+      <main>
+        <CourseList />
+      </main>
+      <footer>
+        <FooterSection />
+      </footer>
+    </>
+  );
 };
 
 export default Courses;
