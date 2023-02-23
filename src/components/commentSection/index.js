1ec863aa993d81264fd304594a3c3c1b8804317b
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2023-02-23 20:24:33 +0330

    add forData

--- src/components/commentSection/index.js
+++ src/components/commentSection/index.js
@@ -1,13 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import Comment from './comment';
-import Button from '../UI/Button';
+import React, { useState, useEffect } from "react";
+import Comment from "./comment";
+import Button from "../UI/Button";
 function CommentSection() {
   const [comments, setComments] = useState([]);
-
+  const formData = [
+    {
+      id: 1,
+      formID: "author",
+      type: "text",
+      value: null,
+      name: "fullName",
+      label: "نام :",
+      placeHolder: "لطفا نام و نام خانوادگی خود را وارد کنید ...",
+      className: null,
+    },
+    {
+      id: 2,
+      formID: "email",
+      type: "email",
+      value: null,
+      name: "emailAddress",
+      label: "ایمیل :",
+      placeHolder: "لطفا ایمیل خود را وارد کنید ...",
+      className: null,
+    },
+    {
+      id: 3,
+      formID: "text",
+      type: "textarea",
+      value: null,
+      name: "comment",
+      label: "نظر :",
+      placeHolder: "لطفا نظر خود را وارد کنید ...",
+      className: null,
+    },
+  ];
   useEffect(() => {
-    fetch('http://localhost:5000/api/comments/')
-      .then(response => response.json())
-      .then(data => {
+    fetch("http://localhost:5000/api/comments/")
+      .then((response) => response.json())
+      .then((data) => {
         setComments(data);
       });
   }, []);
@@ -15,36 +46,32 @@ function CommentSection() {
   function handleSubmit(event) {
     event.preventDefault();
     const data = {
-        "postId": "123fsfdfsafrt7",
-        "username": event.target[0].value,
-        "email": event.target[1].value,
-        "comment": event.target[2].value
+      postId: "123fsfdfsafrt7",
+      username: event.target[0].value,
+      email: event.target[1].value,
+      comment: event.target[2].value,
     };
-    fetch('http://localhost:5000/api/comments/send', {
-      method: 'POST',
+    fetch("http://localhost:5000/api/comments/send", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'User-Agent': 'PostmanRuntime/7.31.0',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive'
+        "Content-Type": "application/json",
+        "User-Agent": "PostmanRuntime/7.31.0",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .then(res => {
+      .then((response) => response.json())
+      .then((res) => {
         console.log(res);
-        setComments(prevComments => [...prevComments, res]);
-    });
+        setComments((prevComments) => [...prevComments, res]);
+      });
   }
 
-  const commentElements = comments.map((comment,index) => {
+  const commentElements = comments.map((comment, index) => {
     return (
-      <Comment
-        key={index}
-        author={comment.username}
-        text={comment.comment}
-      />
+      <Comment key={index} author={comment.username} text={comment.comment} />
     );
   });
 
@@ -54,20 +81,21 @@ function CommentSection() {
       <div className="comment-list">{commentElements}</div>
       <form onSubmit={handleSubmit} className="comment-form">
         <h3>لطفا یک نظر ایجاد کنید </h3>
-        <div className="form-group">
-          <label htmlFor="author">نام :</label>
-          <input type="text" id="author" name="author" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">ایمیل :</label>
-          <input type="text" id="email" name="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="text">نظر :</label>
-          <textarea id="text" name="text"></textarea>
-        </div>
-        {/* <input type="submit" value="ارسال" className='subBtn' /> */}
-        <Button color="main" type="submit">ارسال</Button>
+        {formData.map((item) => {
+          return (
+            <div className="form-group">
+              <label htmlFor={item.formID}>{item.label}</label>
+              {item.type !== "textarea" ? (
+                <input type={item.type} id={item.formID} name={item.formID} placeholder={item.placeHolder} />
+              ) : (
+                <textarea id={item.formID} name={item.formID}></textarea>
+              )}
+            </div>
+          );
+        })}
+        <Button color="main" type="submit">
+          ارسال
+        </Button>
       </form>
     </div>
   );
